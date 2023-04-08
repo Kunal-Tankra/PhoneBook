ab3341e8432cf776df9e4803ba0fc1de42a53320
Kunal Tankra <kunaltankra2002@gmail.com>
2023-04-08 12:01:56 +0530

    fixed bug of edint

--- src/components/Table.js
+++ src/components/Table.js
@@ -79,7 +79,7 @@ const Table = () => {
     let warning = false
 
     for (const data of tableData) {
-      if (data.name === nameEdit || data.num === numEdit) {
+      if ((id !== data.id) &&( data.name === nameEdit || data.num === numEdit)) {
         warning = true;
         break;
       }
